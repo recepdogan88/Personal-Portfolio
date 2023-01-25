c5f87b2f0c2cd3185b65d90cd49921132781ed7e
Recep Dogan <rdogan8891@gmail.com>
2023-01-25 11:58:38 +0100

    text animation has been added

---
+++ js/animation.js
@@ -0,0 +1,6 @@
+const typed = new Typed(".animated-word", {
+    strings: ["Recep Dogan", " a Web Developer","Cloud Practitioner" ,"from Turkey"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+  });
