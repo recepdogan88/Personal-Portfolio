30aa1ba63649e934e70c13e76b8469b3b49cf336
Recep Dogan <rdogan8891@gmail.com>
2023-03-10 10:49:34 +0100

    animation has been changed

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,5 @@
 const typed = new Typed(".animated-word", {
-    strings: ["Recep Dogan", " a Web Developer","Cloud Practitioner" ,"from Turkey"],
+    strings: ["Recep Dogan","a Solutions Architect" , " a Web Developer","from Turkey"],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true,
